62636f4c8a2c668e0b9e3eb0598a6f596dd2184e
Александр <sasha.axm@yandex.ru>
2016-06-30 20:35:03 +0300

    Описание формы

---
+++ index.php
@@ -0,0 +1,63 @@
+<html><head> <meta charset="utf-8">
+
+
+    <title> Ахметзянов А.В. </title>
+
+
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="expires" content="0">
+</head>
+<body>
+
+<div align="center"><h1>Гостевая книга</h1></div>
+
+#Создание формы
+<form action="index.php" method="post">
+    <table width="500" cellpadding="2" cellspacing="0" style="border: 1px solid rgb(0, 75, 150);" bgcolor="#D2B48C">
+        <tbody><tr>
+            <td align="right">
+                * Имя:
+            </td>
+            <td align="left">
+                <input type="text" name="msg_from" maxlength="40" size="30">
+            </td>
+        </tr>
+        <tr>
+            <td align="right">
+                E-Mail:
+            </td>
+            <td align="left">
+                <input type="text" name="msg_mail" maxlength="40" size="30">
+            </td>
+        </tr>
+        <tr>
+            <td align="right">
+                ** URL:
+            </td>
+            <td align="left">
+                <input type="text" name="msg_url" maxlength="40" size="30">
+            </td>
+        </tr>
+        <tr>
+            <td align="right">
+                * Сообщение:
+            </td>
+            <td align="left">
+                <textarea cols="45" rows="7" name="msg_message"></textarea>
+            </td>
+        </tr>
+        <tr>
+            <td align="center" colspan="2">
+                <input type="submit" name="msg_submit" value="Выполнить">
+                <input type="reset">
+            </td>
+        </tr>
+        <tr>
+            <td align="center" colspan="2">
+
+            </td>
+        </tr>
+        </tbody></table>
+</form>
+</body>
+</html>
